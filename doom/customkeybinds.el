170ecf7ae97937c979b36dc0122de072f1ae360a
Gaurav Raj <techw803@gmail.com>
2025-01-30 02:38:01 +0530

    Added updated doomemacs dots

--- doom/customkeybinds.el
+++
@@ -1,5 +0,0 @@
-;; Custom Keybindings
-(use-package evil-nerd-commenter
-  :bind ("M-/" . evilnc-comment-or-uncomment-lines)) ;; using M-/ for comments
-
-(map! :leader :desc "Visit Buffer" "b o" #'ibuffer-visit-buffer) ;; open buffer from ibuffer listing
