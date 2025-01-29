2136c415d1fc37cb90c9519a618dacf91ed84274
Gaurav Raj <techw803@gmail.com>
2025-01-30 02:10:32 +0530

    Added updated doomemacs dots

--- doom.bak/.config/doom/customkeybinds.el
+++
@@ -1,6 +0,0 @@
-;; Custom Keybindings
-(use-package evil-nerd-commenter
-  :bind ("M-/" . evilnc-comment-or-uncomment-lines)) ;; using M-/ for comments
-
-(map! :leader :desc "Visit Buffer" "b o" #'ibuffer-visit-buffer) ;; open buffer from ibuffer listing
-(map! :map emmet-mode-keymap "TAB" #'emmet-expand-line) ;; tab completion for emmet
